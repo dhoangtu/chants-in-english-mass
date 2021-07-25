8aa238ace91a6e2c78c41c9b7382b091da048463
dhtu <dhoangtu@gmail.com>
2021-07-25 13:24:49 +0700

    xuat ban

--- book/publish-book.sh
+++ book/publish-book.sh
@@ -7,14 +7,12 @@ DELIMITER="@"
 GEN=./pdf-generated
 mkdir ${GEN}
 
-RESIZED=./resized-90
-mkdir ${RESIZED}
-
 CONTENT=./content-table.csv
 rm -rf ${CONTENT}
 
 # scan .ly files
 pagecounter=1
+filelist=()
 for fullname in ${LYFILES}/*.ly
 do
     echo "=== Generating $fullname "
@@ -34,8 +32,7 @@ do
     #echo "composer: ${composer}"
     
     # generate pdf files
-    #lilypond --output="${GEN}/${title}${DELIMITER}${composer}" -dno-point-and-click --pdf "$fullname"
-
+    lilypond --output="${GEN}/${shortname}" -dno-point-and-click --pdf "$fullname"
     
     #page counter
     echo "${title};${pagecounter}" >> ${CONTENT}
@@ -44,45 +41,20 @@ do
     echo "${title}${DELIMITER}${composer}.pdf : ${pageno} pages"
     pagecounter=$(( $pageno + $pagecounter ))
     
+    filelist+=( "${GEN}/${shortname}.pdf" )
+    
     echo "" 
 done
 
-# resize all pdf
-filelist=()
-for fullname in ${GEN}/*.pdf
-do
-   echo "=== Resizing $fullname "
-   name=`basename -- "$fullname"`
-   shortname="${name%.*}"
-   
- #~/Desktop/software/pdfScale.sh -v -r a5 -s 0.90 "${fullname}" "${RESIZED}/${name}"
- 
-  filelist+=( "${RESIZED}/${name}" )
-done
-
 # combine songs
-#pdftk "${filelist[@]}" cat output songs.pdf
+pdftk "${filelist[@]}" cat output songs.pdf
 
-# add page numbers
+# add page numbers, adjust inner/outer margins of odd/even pages, scale pages
 pdflatex song-odd-even.tex
 
-# combine all pdf files in name order
-PREFACEVN=./preface-vn.pdf
-PREFACEEN=./preface-en.pdf
-REFVN=./ref-vn.pdf
-REFEN=./ref-en.pdf
-TABLE=./contents.pdf
-BLANK=./blank-a5.pdf
-
-pdftk ${PREFACEVN} ${PREFACEEN} song-odd-even.pdf ${TABLE} ${BLANK} cat output song-book.pdf
-
-# adjust even-odd page
-pdfjam --twoside --paper a5paper --offset '0.3cm 0cm' song-book.pdf --outfile book-adjusted.pdf
-
-FRONT=./cover-front.pdf
-INNER=./cover-inner.pdf
-BACK=./cover-back.pdf
-pdftk ${FRONT} ${INNER} ${BLANK} book-adjusted.pdf ${BACK} cat output chants-in-english-mass.pdf
+# add covers
+pdftk cover-front.pdf cover-inner.pdf blank-a5.pdf preface-vn.pdf preface-en.pdf song-odd-even.pdf \
+  contents.pdf blank-a5.pdf ref-vn.pdf blank-a5.pdf ref-en.pdf cover-back.pdf cat output chants-in-english-mass.pdf
 
 # remove temporary files
 #rm -rf ${GEN} ${RESIZED} songs.pdf song-odd-even.pdf song-book.pdf book-adjusted.pdf
